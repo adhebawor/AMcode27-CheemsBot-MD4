0d591e75a71f9a6835e2675f38c55fcbebcc61d4
AMcode27 <107024251+adhebawor@users.noreply.github.com>
2022-09-18 08:41:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,4 @@
-//════════════════════════════//
-//If you want to recode, reupload
-//or copy the codes/script,
-//pls give credit
-//no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
-//════════════════════════════//
-//recode kar ke youtube pe upload kar rhe hai ya
-//codes copy kar ke apne script me dal rhe
-//hai to, description me xeon ka yt channel
-// ka link paste kr dena as a cradit or github 
-//repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
-//════════════════════════════//
-//If you recode and uploading on your channel
-//or copy pasting the codes in ur script, 
-//i give permission to do as long as you
-//put Xeons youtube channel link in the video
-//description and tag me on githuh repo, 
-//thank you🦄
+
 //════════════════════════════//
 
 const fs = require('fs')
@@ -43,7 +23,7 @@ global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/lv_7144190272958713089_20220918015847.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -54,32 +34,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6282138037447'] //ur owner number
+global.ownername = "🪀◥དAdheBaworཌ◤" //ur owner name
+global.ytname = "YT: Jas Biru Langit" //ur yt chanel name
+global.socialm = "GitHub: AMcode27" //ur github or insta name
+global.location = "Indonesia, JATENG, Cilacap" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['6282138037447']
+global.ownertag = '6282138037447' //ur tag number
+global.botname = '꧁ঔৣ☬AMͥcͣoͫde27☬ঔৣ꧂' //ur bot name
+global.linkz = "https://youtube.com/c/JasBiruLangit?sub_confirmation=1" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.amcode27.my.id/2022/03/membuat-feed-instagram-nyambung-dengan-canva.html" //ur website to be displayed
+global.botscript = 'https://github.com/adhebawor/AMcode27-CheemsBot-MD4' //script link
+global.reactmoji = "🪀" //ur menu react emoji
+global.themeemoji = "🪀" //ur theme emoji
+global.packname = "ঔৣ☬AMͥcͣoͫde27☬ঔৣ🦄🦄🦄 " //ur sticker watermark packname
+global.author = "ঔৣ☬AMͥcͣoͫde27☬ঔৣ" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("https://telegra.ph/file/1c2668a1f2cf1dd1d44e3.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("https://telegra.ph/file/1c2668a1f2cf1dd1d44e3.jpg") //ur logo pic
+global.err4r = fs.readFileSync("https://telegra.ph/file/1c2668a1f2cf1dd1d44e3.jpg") //ur error pic
+global.thumb = fs.readFileSync("https://telegra.ph/file/1c2668a1f2cf1dd1d44e3.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6285211112701'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -101,28 +81,28 @@ global.ntilink = []
 
 //omther
 global.sessionName = 'session'
-global.antitags = true
+global.antitags = false
 global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+global.sp = '﴿☞'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'Selesai Nih Broo ✓',
+    admin: 'Fitur Ini Hanya Untuk Admin!',
+    botAdmin: 'Bot Harus Admin Dulu!',
+    owner: 'Fitur Ini Hanya Untuk Pemilik BOT!',
+    group: 'Fitur Hanya Digunakan Untuk Grup!',
+    private: 'Fitur Hanya Digunakan Untuk Obrolan Pribadi!',
+    bot: 'Fitur Ini Hanya Untuk Bot',
+    wait: '🚀Otw Proses Brooo...',
+    linkm: 'Mana linknya Broo?🧐',
+    error: 'Tidak Bisa Dijangkau Broo!!',
+    endLimit: 'Batas Harian (Limit) Anda Telah Kedaluwarsa, Batas Akan Direset Setiap Jam 6 Sore',
+    ban: 'Anda telah diblokir oleh Owner, Ketik .owner Agar di aktifkan Kembali.',
+    nsfw: 'Fitur nsfw belum diaktifkan, silahkan hubungi Owner untuk aktivasi',
+    banChat: 'Bot diblokir di grup ini, harap hubungi pemiliknya untuk membatalkan pemblokiran'
 }
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 20,
     monayawal: 1000
 }
    global.rpg = {
@@ -133,7 +113,7 @@ global.mess = {
    umpanawal: 5,
    potionawal: 1
 }
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.thumb = fs.readFileSync('./https://telegra.ph/file/1c2668a1f2cf1dd1d44e3.jpg')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
